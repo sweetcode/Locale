775ea209337adf2ab0946b125ed016c952bf99c2
Kareem Morsy <kreem.morsy@hotmail.com>
2016-01-10 13:19:21 +0200

    Add arabic translation

---
+++ Arabic.php
@@ -0,0 +1,115 @@
+<?php
+// Global
+$locale['Global']['Selector']['Title'] = 'العربية';
+$locale['Global']['Header']['Title'] = "أضخم موقع API صمم لسيرفرات ومواقع Minecraft";
+$locale['Global']['Header']['Description'] = "MCAPI العديد من الـ APIs التى تستخدم من قبل المواقع المتعلقة بـ minecraft. جميع الـ APIs تخرج صور او تنسيقات json على حسب نوع الـ API.";
+$locale['Global']['Networks']['Title'] = "Networks utilising MCAPI";
+$locale['Global']['Footer']['Copyright'] = "&copy; MCAPI | <a href=\"https://minecraft.net\">Minecraft</a> مملوكة من قبل <a href=\"https://mojang.com\">Mojang</a> وليست تابعة لهذا الموقع.";
+$locale['Global']['Footer']['Issue'] = "متتبع الأخطاء";
+$locale['Global']['Footer']['Sponsored'] = "برعاية MineSuperior &amp; MinePay";
+// Homepage [Start]
+$locale['Homepage']['Table']['Title'] = "ما هى الـ APIs المتوفرة فى MCAPI?";
+// Table Headers
+$locale['Homepage']['Table']['Headers']['API'] = "API";
+$locale['Homepage']['Table']['Headers']['Description'] = "الوصف";
+$locale['Homepage']['Table']['Headers']['Path'] = "المسار";
+$locale['Homepage']['Table']['Headers']['Output'] = "المخرج";
+$locale['Homepage']['Table']['Headers']['Example'] = "المثال";
+$locale['Homepage']['Table']['Headers']['Caching'] = "التخزين المؤقت";
+// Table Body - Avatar API
+$locale['Homepage']['Table']['Body']['Avatar']['API'] = "Avatar API";
+$locale['Homepage']['Table']['Body']['Avatar']['Description'] = "API يحول الـ Skin الخاص باللاعب الى صورة شخصية.";
+$locale['Homepage']['Table']['Body']['Avatar']['Path'] = "/avatar";
+$locale['Homepage']['Table']['Body']['Avatar']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Avatar']['Example'] = "/examples/avatar-api";
+$locale['Homepage']['Table']['Body']['Avatar']['Caching'] = "دقيقة واحدة";
+// Table Body - Skin API
+$locale['Homepage']['Table']['Body']['Skin']['API'] = "Skin API";
+$locale['Homepage']['Table']['Body']['Skin']['Description'] = "API لطباعة الـ Skin الخاص باللاعب.";
+$locale['Homepage']['Table']['Body']['Skin']['Path'] = "/skin";
+$locale['Homepage']['Table']['Body']['Skin']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Skin']['Example'] = "/examples/skin-api";
+$locale['Homepage']['Table']['Body']['Skin']['Caching'] = "دقيقة واحدة";
+// Table Body - Server Query API
+$locale['Homepage']['Table']['Body']['Query']['API'] = "Server Query API";
+$locale['Homepage']['Table']['Body']['Query']['Description'] = "API لطباعة البيانات عن السيرفر.";
+$locale['Homepage']['Table']['Body']['Query']['Path'] = "/query";
+$locale['Homepage']['Table']['Body']['Query']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Query']['Example'] = "/examples/query-api";
+$locale['Homepage']['Table']['Body']['Query']['Caching'] = "دقيقة واحدة";
+// Table Body - UUID API
+$locale['Homepage']['Table']['Body']['UUID']['API'] = "UUID API";
+$locale['Homepage']['Table']['Body']['UUID']['Description'] = "لطباعة معلومات الـ UUID.";
+$locale['Homepage']['Table']['Body']['UUID']['Path'] = "/uuid";
+$locale['Homepage']['Table']['Body']['UUID']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['UUID']['Example'] = "/examples/uuid-api";
+$locale['Homepage']['Table']['Body']['UUID']['Caching'] = "دقيقة واحدة";
+// Table Body - Name API
+$locale['Homepage']['Table']['Body']['Name']['API'] = "Name API";
+$locale['Homepage']['Table']['Body']['Name']['Description'] = "يطبع معلومات عن الاسم.";
+$locale['Homepage']['Table']['Body']['Name']['Path'] = "/name";
+$locale['Homepage']['Table']['Body']['Name']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Name']['Example'] = "/examples/name-api";
+$locale['Homepage']['Table']['Body']['Name']['Caching'] = "دقيقة واحدة";
+// Table Body - Name History API
+$locale['Homepage']['Table']['Body']['History']['API'] = "Name History API";
+$locale['Homepage']['Table']['Body']['History']['Description'] = "يطبع معلومات عن سجل الاسماء الخاص باللاعب.";
+$locale['Homepage']['Table']['Body']['History']['Path'] = "/history";
+$locale['Homepage']['Table']['Body']['History']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['History']['Example'] = "/examples/name-history-api";
+$locale['Homepage']['Table']['Body']['History']['Caching'] = "دقيقة واحدة";
+// Table Body - MinecraftMarket API
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['API'] = "MinecraftMarket API";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Description'] = "يطبع معلومات عن متاجر MinecraftMarket.";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Path'] = "/minecraftmarket";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Example'] = "/examples/minecraftmarket-api";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Caching'] = "دقيقة واحدة";
+// Table Body - Buycraft API
+$locale['Homepage']['Table']['Body']['Buycraft']['API'] = "Buycraft API";
+$locale['Homepage']['Table']['Body']['Buycraft']['Description'] = "يطبع معلومات عن متاجر Buycraft.";
+$locale['Homepage']['Table']['Body']['Buycraft']['Path'] = "/buycraft";
+$locale['Homepage']['Table']['Body']['Buycraft']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Buycraft']['Example'] = "/examples/buycraft-api";
+$locale['Homepage']['Table']['Body']['Buycraft']['Caching'] = "دقيقة واحدة";
+// Table Body - Other APIs
+$locale['Homepage']['Table']['Body']['Other']['API'] = "Other APIs";
+$locale['Homepage']['Table']['Body']['Other']['Description'] = "مجموعة مختلفة من الـ APIs التى ليست فى فئة معينة.";
+$locale['Homepage']['Table']['Body']['Other']['Path'] = "/other";
+$locale['Homepage']['Table']['Body']['Other']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Other']['Example'] = "/examples/other-apis";
+$locale['Homepage']['Table']['Body']['Other']['Caching'] = "دقيقة واحدة";
+// Homepage [End]
+// Example Pages [Start]
+// Global locale for pages
+$locale['GlobalPages']['Multiple']['Usage'] = "أمثلة للإستخدامات";
+$locale['GlobalPages']['Single']['Usage'] = "مثال للاستخدام";
+// Avatar API Page
+$locale['Pages']['Avatar']['Title'] = "Avatar API";
+$locale['Pages']['Avatar']['Description'] = "هذا الـ API يعطى صورة لرأس اللاعب ثنائية او ثلاثية الأبعاد.";
+// Skin API Page
+$locale['Pages']['Skin']['Title'] = "Skin API";
+$locale['Pages']['Skin']['Description'] = "هذا الـ API يعطى صورة لجسم اللاعب ثنائية أو ثلاثية الأبعاد.";
+// Server Query API	Page
+$locale['Pages']['Query']['Title'] = "Server Query API";
+$locale['Pages']['Query']['Description'] = "هذا الـ API يجلب المعلومات من سيرفرات minecraft مثل عدد اللاعبين المتواجدين.";
+// UUID API Page
+$locale['Pages']['UUID']['Title'] = "UUID API";
+$locale['Pages']['UUID']['Description'] = "هذا الـ API يعطيك الـ UUID لمستخدم أو عدة مسخدمين فى نفس الوقت.";
+// Name API Page
+$locale['Pages']['Name']['Title'] = "Name API";
+$locale['Pages']['Name']['Description'] = "هذا الـ API يعطيك اسم صاحب الـ UUID المعطى.";
+// Name History API Page
+$locale['Pages']['History']['Title'] = "Name History API";
+$locale['Pages']['History']['Description'] = "قريباً";
+// MinecraftMarket API Page
+$locale['Pages']['MinecraftMarket']['Title'] = "MinecraftMarket API";
+$locale['Pages']['MinecraftMarket']['Description'] = "قريباً";
+// Buycraft API Page
+$locale['Pages']['Buycraft']['Title'] = "Buycraft API";
+$locale['Pages']['Buycraft']['Description'] = "هذا الـ API يستخدم لجلب معلومات عن متجر buycraft.";
+// Other APIs Page
+$locale['Pages']['Other']['Title'] = "Other APIs";
+$locale['Pages']['Other']['Description'] = "هذه الـ APIs هى مجموعة من الـ APIs العشوائية تماماً او لا تحتاج إلى أن تكون فى فئة معينة.";
+// Example Pages [End]
+?>

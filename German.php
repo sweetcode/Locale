3372ee19245681f1da760ae590e4e8e9fbfe1ca5
sweetcode <jan@krueger-jan.de>
2016-07-07 01:57:41 +0200

    German Translation

---
+++ German.php
@@ -0,0 +1,166 @@
+<?php
+
+// Global
+
+$locale['Global']['Selector']['Title'] = 'Deutsch';
+$locale['Global']['Header']['Title'] = "Kraftvolle API Seite entworfen für Minecraft Server & Webseiten";
+$locale['Global']['Header']['Description'] = "MCAPI bietet viele unterschiedliche APIs die für Minecraft Websiten gedacht sind. All diese APIs geben entwender ein Bild oder JSON zurück, dies ist abhängig von der Art der API.";
+$locale['Global']['Networks']['Title'] = "Netzwerke die McAPI nutzen";
+$locale['Global']['Footer']['Copyright'] = "&copy; MCAPI | <a href=\"https://minecraft.net\">Minecraft</a> steht unter dem Copyright von <a href=\"https://mojang.com\">Mojang</a> und steht in keiner Verbindung zu dieser Seite.";
+$locale['Global']['Footer']['Issue'] = "Issue Tracker";
+$locale['Global']['Footer']['Sponsored'] = "Gesponsert von MineSuperior &amp; MinePay";
+
+// Homepage [Start]
+
+$locale['Homepage']['Table']['Title'] = "Welche APIs bietet McAPI an?";
+
+// Table Headers
+
+$locale['Homepage']['Table']['Headers']['API'] = "API";
+$locale['Homepage']['Table']['Headers']['Description'] = "Beschreibung";
+$locale['Homepage']['Table']['Headers']['Path'] = "Pfad";
+$locale['Homepage']['Table']['Headers']['Output'] = "Ausgabe";
+$locale['Homepage']['Table']['Headers']['Example'] = "Beispiel";
+$locale['Homepage']['Table']['Headers']['Caching'] = "Caching";
+
+// Table Body - Avatar API
+
+$locale['Homepage']['Table']['Body']['Avatar']['API'] = "Avatar API";
+$locale['Homepage']['Table']['Body']['Avatar']['Description'] = "API um Skins in Avatare umzuwandeln.";
+$locale['Homepage']['Table']['Body']['Avatar']['Path'] = "/avatar";
+$locale['Homepage']['Table']['Body']['Avatar']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Avatar']['Example'] = "/examples/avatar-api";
+$locale['Homepage']['Table']['Body']['Avatar']['Caching'] = "1 Minute";
+
+// Table Body - Skin API
+
+$locale['Homepage']['Table']['Body']['Skin']['API'] = "Skin API";
+$locale['Homepage']['Table']['Body']['Skin']['Description'] = "API um das Charakter Modell eines Spielers darzustellen.";
+$locale['Homepage']['Table']['Body']['Skin']['Path'] = "/skin";
+$locale['Homepage']['Table']['Body']['Skin']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Skin']['Example'] = "/examples/skin-api";
+$locale['Homepage']['Table']['Body']['Skin']['Caching'] = "1 Minute";
+
+// Table Body - Server Query API
+
+$locale['Homepage']['Table']['Body']['Query']['API'] = "Server Query API";
+$locale['Homepage']['Table']['Body']['Query']['Description'] = "API die Informationen über einen Server ausigbt.";
+$locale['Homepage']['Table']['Body']['Query']['Path'] = "/query";
+$locale['Homepage']['Table']['Body']['Query']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Query']['Example'] = "/examples/query-api";
+$locale['Homepage']['Table']['Body']['Query']['Caching'] = "1 Minute";
+
+// Table Body - UUID API
+
+$locale['Homepage']['Table']['Body']['UUID']['API'] = "UUID API";
+$locale['Homepage']['Table']['Body']['UUID']['Description'] = "Gibt Informationen über eine UUID aus.";
+$locale['Homepage']['Table']['Body']['UUID']['Path'] = "/uuid";
+$locale['Homepage']['Table']['Body']['UUID']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['UUID']['Example'] = "/examples/uuid-api";
+$locale['Homepage']['Table']['Body']['UUID']['Caching'] = "1 Minute";
+
+// Table Body - Name API
+
+$locale['Homepage']['Table']['Body']['Name']['API'] = "Name API";
+$locale['Homepage']['Table']['Body']['Name']['Description'] = "Gibt Informationen über einen Namen aus.";
+$locale['Homepage']['Table']['Body']['Name']['Path'] = "/name";
+$locale['Homepage']['Table']['Body']['Name']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Name']['Example'] = "/examples/name-api";
+$locale['Homepage']['Table']['Body']['Name']['Caching'] = "1 Minute";
+
+// Table Body - Name History API
+
+$locale['Homepage']['Table']['Body']['History']['API'] = "Name History API";
+$locale['Homepage']['Table']['Body']['History']['Description'] = "Gibt alle Namen des Benutzers zurück, die er je angenommen hatte.";
+$locale['Homepage']['Table']['Body']['History']['Path'] = "/history";
+$locale['Homepage']['Table']['Body']['History']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['History']['Example'] = "/examples/name-history-api";
+$locale['Homepage']['Table']['Body']['History']['Caching'] = "1 Minute";
+
+// Table Body - MinecraftMarket API
+
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['API'] = "MinecraftMarket API";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Description'] = "Gibt Informationen über MinecraftMarket Geschäfte aus.";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Path'] = "/minecraftmarket";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Example'] = "/examples/minecraftmarket-api";
+$locale['Homepage']['Table']['Body']['MinecraftMarket']['Caching'] = "1 Minute";
+
+// Table Body - Buycraft API
+
+$locale['Homepage']['Table']['Body']['Buycraft']['API'] = "Buycraft API";
+$locale['Homepage']['Table']['Body']['Buycraft']['Description'] = "Gibt Informationen über Buycraft Geschäfte aus.";
+$locale['Homepage']['Table']['Body']['Buycraft']['Path'] = "/buycraft";
+$locale['Homepage']['Table']['Body']['Buycraft']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Buycraft']['Example'] = "/examples/buycraft-api";
+$locale['Homepage']['Table']['Body']['Buycraft']['Caching'] = "1 Minute";
+
+// Table Body - Other APIs
+
+$locale['Homepage']['Table']['Body']['Other']['API'] = "Other APIs";
+$locale['Homepage']['Table']['Body']['Other']['Description'] = "Viele unterschiedliche APIs, denen keine Kategorie zugewiesen werden konnte.";
+$locale['Homepage']['Table']['Body']['Other']['Path'] = "/other";
+$locale['Homepage']['Table']['Body']['Other']['Output'] = "PNG";
+$locale['Homepage']['Table']['Body']['Other']['Example'] = "/examples/other-apis";
+$locale['Homepage']['Table']['Body']['Other']['Caching'] = "1 Minute";
+
+// Homepage [End]
+
+// Example Pages [Start]
+
+// Global locale for pages
+
+$locale['GlobalPages']['Multiple']['Usage'] = "Beispiel Nutzung";
+$locale['GlobalPages']['Single']['Usage'] = "Beispiel Nutzung";
+
+// Avatar API Page
+
+$locale['Pages']['Avatar']['Title'] = "Avatar API";
+$locale['Pages']['Avatar']['Description'] = "Diese API gibt dir die Möglichkeit, den Kopf eines Spielers in 2D oder in 3D darzustellen.";
+
+// Skin API Page
+
+$locale['Pages']['Skin']['Title'] = "Skin API";
+$locale['Pages']['Skin']['Description'] = "This API provides you the ability to get a image of the character model of a player in either 2D or 3D.";
+$locale['Pages']['Skin']['Description'] = "Diese API gibt dir die Möglichkeit, das Charakter Modell eines Spielers in 2D oder in 3D darzustellen.";
+
+// Server Query API	Page
+
+$locale['Pages']['Query']['Title'] = "Server Query API";
+$locale['Pages']['Query']['Description'] = "Diese API gibt dir die Möglichkeit Informationen von einem Minecraft Server, wie z.B. die Anzahl an Spielern, zuerhalten.";
+
+// UUID API Page
+
+$locale['Pages']['UUID']['Title'] = "UUID API";
+$locale['Pages']['UUID']['Description'] = "Diese API kann die UUIDs eines oder mehrerer Spieler gleichzeitig zurückgeben.";
+
+// Name API Page
+
+$locale['Pages']['Name']['Title'] = "Name API";
+$locale['Pages']['Name']['Description'] = "This API is for getting the name for a UUID.";
+$locale['Pages']['Name']['Description'] = "Diese API löst eine UUID nach seinen passenden Namen auf.";
+
+// Name History API Page
+
+$locale['Pages']['History']['Title'] = "Name History API";
+$locale['Pages']['History']['Description'] = "Kommt bald";
+
+// MinecraftMarket API Page
+
+$locale['Pages']['MinecraftMarket']['Title'] = "MinecraftMarket API";
+$locale['Pages']['MinecraftMarket']['Description'] = "Kommt bald";
+
+// Buycraft API Page
+
+$locale['Pages']['Buycraft']['Title'] = "Buycraft API";
+$locale['Pages']['Buycraft']['Description'] = "This API is used for getting data about a buycraft store.";
+$locale['Pages']['Buycraft']['Description'] = "This API wird dazu benutzt um Daten eines Buycraft Geschäftes zubekommen.";
+
+// Other APIs Page
+
+$locale['Pages']['Other']['Title'] = "Other APIs";
+$locale['Pages']['Other']['Description'] = "Diese APIs haben viele unterschiedliche Aufgabenbereiche oder brauchen keine eigene Kategorie.";
+
+// Example Pages [End]
+
+?>
